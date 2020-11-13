0a43ac4ae5c7bb599d21ee7f67d1cabeac62947a
Subhadeep <subhadeep290187@gmail.com>
2020-11-13 21:06:00 +0530

    perrfomance time

--- Product-of-array-except-self/Product-of-array-except-self.js
+++ Product-of-array-except-self/Product-of-array-except-self.js
@@ -15,7 +15,9 @@ const product_of_array_except_self_n2 = (arr)=>{
     });
     return newArr;
 }
-
+const t0 =  performance.now();
+console.log("with O(n^2)", product_of_array_except_self_n2(arr), 
+"Execution Time =", performance.now()-t0);
 // with O(n)
 const product_of_array_except_self_n_1 = (arr)=>{
     let product = 1;
@@ -28,4 +30,6 @@ const product_of_array_except_self_n_1 = (arr)=>{
     })
     return output;
 }
-console.log(product_of_array_except_self_n_1(arr));
+const t1 =  performance.now();
+console.log("With O(n)",product_of_array_except_self_n_1(arr),
+"Execution Time =", performance.now()-t1);
